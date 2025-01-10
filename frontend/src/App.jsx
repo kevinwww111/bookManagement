f8c4d6530abb347f42ac8af54078c7a6312432c7
kevinwww111 <kevinwg114@gmail.com>
2025-01-10 20:37:51 +0800

    App

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import AddBook from "./pages/AddBook";
+import EditBook from "./pages/EditBook";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-book" element={<AddBook />} />
+        <Route path="/edit-book/:id" element={<EditBook />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
